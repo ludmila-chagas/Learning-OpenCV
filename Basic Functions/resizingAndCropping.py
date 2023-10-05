1664bf948bb77c4aa2aa9fe09ea642578b97b224
Ludmila <ghansth@gmail.com>
2023-10-05 11:13:13 -0300

    First commit: image resize and crop

---
+++ Basic Functions/resizingAndCropping.py
@@ -0,0 +1,27 @@
+# This script reads an image, resize its dimension and crops its size.
+
+import cv2
+import numpy as np
+
+# Read the image
+img = cv2.imread("Resources/gatin.png")
+
+# Show image dimensions 
+print(img.shape)
+
+# Show image (post previous operations)
+cv2.imshow("Original image", img)
+
+# Change the dimensions
+imgResized = cv2.resize(img, (600,350))
+
+# Crop the image
+imgCropped = imgResized[0:350, 0:600]
+
+# Show image (post previous operations)
+cv2.imshow("Image size adjusted", imgCropped)
+
+print(imgCropped.shape)
+
+# Time for image display, in milliseconds
+cv2.waitKey(0)
