34bad554a551df78ed54234ec5049c6de6c106e5
Ludmila <ghansth@gmail.com>
2023-10-06 10:41:32 -0300

    Warp perspective

--- Basic Functions/lines.py
+++ Basic Functions/lines.py
@@ -8,7 +8,7 @@ img = np.zeros((512,512,3),np.uint8)
 cv2.line(img,(100,0),(100,100),(0,255,0),3)
 cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
 cv2.circle(img,(400,50),30,(255,255,0),5)
-cv2.putText(img," OPENCV  ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
+cv2.putText(img," =^.^=  ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3)
 
 
 cv2.imshow("Image",img)
---
+++ Basic Functions/warpPerspective.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/placa.webp")
+cv2.imwrite("Resources/placa.png", img)
+
+width,height = 400,250
+pts1 = np.float32([[212,73],[522,7],[208,281],[497,327]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img,matrix,(width,height))
+
+
+cv2.imshow("Image",img)
+cv2.imshow("Output",imgOutput)
+
+cv2.waitKey(0)
+
+
+# 272,73- left top
+# 522,7- right top
+# 208,281-left bottom
+# 497,327- right bottom
